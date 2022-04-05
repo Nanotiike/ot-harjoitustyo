5270e0cbfe0767239dbb3849967aa5584a6a9495
Harri Gävert <harri.gavert@gmail.com>
2022-04-05 16:48:48 +0300

    budjettiin lisäys

--- Budjetointi-app/src/budjet.py
+++ Budjetointi-app/src/budjet.py
@@ -2,3 +2,9 @@ class budjet:
     def __init__(self, user):
         self.user = user
         self.total = 0
+
+    def add(self, amount: int):
+        self.total += amount
+
+    def substract(self, amount: int):
+        self.total -= amount
