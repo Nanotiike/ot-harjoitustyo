baaaa3c7e5d2eb97fee8ae768b513dcf04d5581a
Harri <harri.gavert@helsinki.fi>
2023-12-22 22:18:42 +0200

    few tests for sudoku_generator

--- src/tests/sudoku_generator_test.py
+++ src/tests/sudoku_generator_test.py
@@ -10,65 +10,60 @@ class TestSudokuGenerator(unittest.TestCase):
         self.assertEqual(len(self.sudoku.board), 9)
         self.assertEqual(len(self.sudoku.board[0]), 9)
     
-    """def test_generate_sudoku_shuffle_numbers(self):
+    def test_generate_sudoku_shuffle_numbers(self):
         self.sudoku.shuffle_numbers()
-        self.assertNotEqual(self.sudoku.board, [[1, 2, 3, 4, 5, 6, 7, 8, 9],
-                                                [4, 5, 6, 7, 8, 9, 1, 2, 3],
-                                                [7, 8, 9, 1, 2, 3, 4, 5, 6],
-                                                [2, 3, 1, 5, 6, 4, 8, 9, 7],
-                                                [5, 6, 4, 8, 9, 7, 2, 3, 1],
-                                                [8, 9, 7, 2, 3, 1, 5, 6, 4],
-                                                [3, 1, 2, 6, 4, 5, 9, 7, 8],
-                                                [6, 4, 5, 9, 7, 8, 3, 1, 2],
-                                                [9, 7, 8, 3, 1, 2, 6, 4, 5]])
+        self.sudoku_player = Sudoku(self.sudoku.board)
+        result = True
+        for i in range(9):
+            for j in range(9):
+                if self.sudoku_player.check_number(i, j, self.sudoku_player.board[i][j]) == False:
+                    result = False
+                    break
+        self.assertEqual(result, True)
         
     def test_generate_sudoku_shuffle_rows(self):
         self.sudoku.shuffle_rows()
-        self.assertNotEqual(self.sudoku.board, [[1, 2, 3, 4, 5, 6, 7, 8, 9],
-                                                [4, 5, 6, 7, 8, 9, 1, 2, 3],
-                                                [7, 8, 9, 1, 2, 3, 4, 5, 6],
-                                                [2, 3, 1, 5, 6, 4, 8, 9, 7],
-                                                [5, 6, 4, 8, 9, 7, 2, 3, 1],
-                                                [8, 9, 7, 2, 3, 1, 5, 6, 4],
-                                                [3, 1, 2, 6, 4, 5, 9, 7, 8],
-                                                [6, 4, 5, 9, 7, 8, 3, 1, 2],
-                                                [9, 7, 8, 3, 1, 2, 6, 4, 5]])
+        self.sudoku_player = Sudoku(self.sudoku.board)
+        result = True
+        for i in range(9):
+            for j in range(9):
+                if self.sudoku_player.check_number(i, j, self.sudoku_player.board[i][j]) == False:
+                    result = False
+                    break
+        self.assertEqual(result, True)
         
     def test_generate_sudoku_shuffle_columns(self):
         self.sudoku.shuffle_columns()
-        self.assertNotEqual(self.sudoku.board, [[1, 2, 3, 4, 5, 6, 7, 8, 9],
-                                                [4, 5, 6, 7, 8, 9, 1, 2, 3],
-                                                [7, 8, 9, 1, 2, 3, 4, 5, 6],
-                                                [2, 3, 1, 5, 6, 4, 8, 9, 7],
-                                                [5, 6, 4, 8, 9, 7, 2, 3, 1],
-                                                [8, 9, 7, 2, 3, 1, 5, 6, 4],
-                                                [3, 1, 2, 6, 4, 5, 9, 7, 8],
-                                                [6, 4, 5, 9, 7, 8, 3, 1, 2],
-                                                [9, 7, 8, 3, 1, 2, 6, 4, 5]])
+        self.sudoku_player = Sudoku(self.sudoku.board)
+        result = True
+        for i in range(9):
+            for j in range(9):
+                if self.sudoku_player.check_number(i, j, self.sudoku_player.board[i][j]) == False:
+                    result = False
+                    break
+        self.assertEqual(result, True)
         
     def test_generate_sudoku_shuffle_3x_rows(self):
         self.sudoku.shuffle_3x_rows()
-        self.assertNotEqual(self.sudoku.board, [[1, 2, 3, 4, 5, 6, 7, 8, 9],
-                                                [4, 5, 6, 7, 8, 9, 1, 2, 3],
-                                                [7, 8, 9, 1, 2, 3, 4, 5, 6],
-                                                [2, 3, 1, 5, 6, 4, 8, 9, 7],
-                                                [5, 6, 4, 8, 9, 7, 2, 3, 1],
-                                                [8, 9, 7, 2, 3, 1, 5, 6, 4],
-                                                [3, 1, 2, 6, 4, 5, 9, 7, 8],
-                                                [6, 4, 5, 9, 7, 8, 3, 1, 2],
-                                                [9, 7, 8, 3, 1, 2, 6, 4, 5]])
+        self.sudoku_player = Sudoku(self.sudoku.board)
+        result = True
+        for i in range(9):
+            for j in range(9):
+                if self.sudoku_player.check_number(i, j, self.sudoku_player.board[i][j]) == False:
+                    result = False
+                    break
+        self.assertEqual(result, True)
         
     def test_generate_sudoku_shuffle_3x_columns(self):
         self.sudoku.shuffle_3x_columns()
-        self.assertNotEqual(self.sudoku.board, [[1, 2, 3, 4, 5, 6, 7, 8, 9],
-                                                [4, 5, 6, 7, 8, 9, 1, 2, 3],
-                                                [7, 8, 9, 1, 2, 3, 4, 5, 6],
-                                                [2, 3, 1, 5, 6, 4, 8, 9, 7],
-                                                [5, 6, 4, 8, 9, 7, 2, 3, 1],
-                                                [8, 9, 7, 2, 3, 1, 5, 6, 4],
-                                                [3, 1, 2, 6, 4, 5, 9, 7, 8],
-                                                [6, 4, 5, 9, 7, 8, 3, 1, 2],
-                                                [9, 7, 8, 3, 1, 2, 6, 4, 5]])"""
+        self.sudoku_player = Sudoku(self.sudoku.board)
+        result = True
+        for i in range(9):
+            for j in range(9):
+                if self.sudoku_player.check_number(i, j, self.sudoku_player.board[i][j]) == False:
+                    result = False
+                    break
+        self.assertEqual(result, True)
         
     def test_validate_sudoku(self):
         self.sudoku.shuffle_numbers()
